f35180c76e6d099884805c3fb0f32d4c4fef3a41
DongokYang <dyang2@academic.rrc.ca>
2023-09-18 13:50:28 -0500

    added CALCULATIONS at sdf assignment file

--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -19,7 +19,11 @@ this_year +=1
 print(f"next year:{this_year} type: {type(this_year)}")
 
 #CALCULATIONS
-
+CANADA_GST_RATE = 0.05
+MANITOBA_PST_RATE = 0.07
+vehicle_price = 22503.03
+price_inclue_tax = vehicle_price + vehicle_price *(CANADA_GST_RATE + MANITOBA_PST_RATE)
+print(f"pre-tax value: {vehicle_price} PST: {vehicle_price*MANITOBA_PST_RATE} GST: {vehicle_price*CANADA_GST_RATE} total: {price_inclue_tax}")
 
 #LISTS
 
