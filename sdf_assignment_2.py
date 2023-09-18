b627a56ab8a8de5dc9cdbf5edc55a5038a249993
DongokYang <dyang2@academic.rrc.ca>
2023-09-18 13:37:48 -0500

    added SIMPLE_DATA_TYPES at sdf assignment file

--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -6,8 +6,20 @@ Usage:
 """
 
 #SIMPLE DATA TYPES
+name = "Dongok"
+print(f"value:{name} type: {type(name)}")
+
+have_Manitoba_drivers_license = False
+print(f"value:{have_Manitoba_drivers_license} type: {type(have_Manitoba_drivers_license)}")
+
+this_year = 2023
+print(f"this year:{this_year} type: {type(this_year)}")
+
+this_year +=1 
+print(f"next year:{this_year} type: {type(this_year)}")
 
 #CALCULATIONS
+
 
 #LISTS
 
